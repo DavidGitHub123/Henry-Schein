3bc932403513f994ee36bdae0f2c3f307f67d2b9
DavidGitHub123 <60446378+DavidGitHub123@users.noreply.github.com>
2022-07-17 18:46:33 -0600

    1st Upload

---
+++ js/js.js
@@ -0,0 +1,93 @@
+
+
+// HS1 customer testimonials page
+
+const testimonials = document.getElementbyID('');
+const product = document.getElementbyID('');
+const segment = document.getElementbyID('');
+const state = document.getElementbyID('');
+const search = document.getElementbyID('');
+
+testimonials.addEventListener('click', () => {
+
+});
+
+
+// HS1 search results page
+
+const testimonials = document.getElementbyID('');
+const product = document.getElementbyID('');
+const segment = document.getElementbyID('');
+const state = document.getElementbyID('');
+const search = document.getElementbyID('');
+const results = document.getElementbyID('');
+
+const previous = document.getElementbyID('');
+const page1 = document.getElementbyID('');
+const page2 = document.getElementbyID('');
+const page3 = document.getElementbyID('');
+const next = document.getElementbyID('');
+
+testimonials.addEventListener('click', () => {
+
+});
+
+
+// login page
+
+const username = document.getElementbyID('');
+const password = document.getElementbyID('');
+
+testimonials.addEventListener('click', () => {
+
+});
+
+
+// database page
+
+const edit = document.getElementbyID('');
+const delete = document.getElementbyID('');
+
+const previous = document.getElementbyID('');
+const page1 = document.getElementbyID('');
+const page2 = document.getElementbyID('');
+const page3 = document.getElementbyID('');
+const next = document.getElementbyID('');
+
+testimonials.addEventListener('click', () => {
+
+});
+
+
+// edit testimonial page
+
+const product = document.getElementbyID('');
+const segment = document.getElementbyID('');
+const state = document.getElementbyID('');
+const testimonials = document.getElementbyID('');
+const name = document.getElementbyID('');
+
+testimonials.addEventListener('click', () => {
+
+});
+
+
+// add new testimonial page
+
+const product = document.getElementbyID('');
+const segment = document.getElementbyID('');
+const state = document.getElementbyID('');
+const testimonials = document.getElementbyID('');
+const name = document.getElementbyID('');
+
+testimonials.addEventListener('click', () => {
+
+});
+
+
+
+
+
+
+
+
